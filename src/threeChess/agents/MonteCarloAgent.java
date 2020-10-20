8d9c5302c3f5616fe2fc751dc31eac3628c80676
Matthew Overington <m.over28@gmail.com>
2020-10-20 16:54:35 +0800

    Created MC agents, ready to start creating selection, runoff and decision functionalities

---
+++ src/threeChess/agents/MonteCarloAgent.java
@@ -0,0 +1,14 @@
+package threeChess.agents;
+
+import threeChess.*;
+
+import java.io.*;
+import java.util.*;
+
+public class RandomMonteCarloAgent {
+
+    public RandomMonteCarloAgent() {
+        
+    }
+
+}
