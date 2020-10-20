62beb4e848492b60dcf292f688ecb9921559692d
Matthew Overington <m.over28@gmail.com>
2020-10-20 20:42:29 +0800

    Added basic structure to MC agent

--- src/threeChess/agents/MonteCarloAgent.java
+++ src/threeChess/agents/MonteCarloAgent.java
@@ -5,10 +5,44 @@ import threeChess.*;
 import java.io.*;
 import java.util.*;
 
-public class RandomMonteCarloAgent {
 
-    public RandomMonteCarloAgent() {
+public class MonteCarloAgent extends Agent {
+
+    private static final String name = "Carlos the Monty";
+    private GameTree gameTree;
+
+    public Position[] playMove(Board board) {
+
+        return null;
+    }
+
+    private void selection() {
+
+    }
+
+    private void expansion() {
         
     }
+
+    private void rollout() {
+        
+    }
+
+    private void backpropogate() {
+        
+    }
+
+    /**
+     * @return the Agent's name, for annotating game description.
+     * **/ 
+    public String toString(){return name;}
+
+    /**
+     * Displays the final board position to the agent, 
+     * if required for learning purposes. 
+     * Other a default implementation may be given.
+     * @param finalBoard the end position of the board
+     * **/
+    public void finalBoard(Board finalBoard){}
 
 }
